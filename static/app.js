f5e3fabd3fadc428a927af0f879a4712e922d447
Michael Governanti <michaelgovernanti@gmail.com>
2025-02-11 15:53:11 -0500

    do i actually hate JS, or do I just hate that it's the wild west

--- static/app.js
+++ static/app.js
@@ -61,14 +61,93 @@ function createBoard(perspective) {
     }
 }
 
+function reverseIds() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i) => {
+        square.setAttribute('square-id', (width * width - 1) - i);
+    })
+}
+
+function revertIds() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i) => {
+        square.setAttribute('square-id', i);
+    })
+}
+
 function changePlayer() {
     if (playerTurn === "dark") {
+        //revertIds(); // only necessary for shared play
         playerTurn = "light";
         playerDisplay.textContent = "light";
     } else {
+        //reverseIds(); // only necessary for shared play
         playerTurn = "dark";
         playerDisplay.textContent = "dark";
     }
+}
+
+function checkIfValidMove(target) {
+    const targetId = Number(target.getAttribute("square-id") || target.parentNode.parentNode.getAttribute('square-id'));
+    const startId = Number(startPositionId);
+    const piece = draggedElement.id;
+    
+    switch(true) {
+        case piece.includes("pawn") :
+            if (piece.includes("light")) {
+                const starterRow = [48,49,50,51,52,53,54,55];
+                if (
+                    (starterRow.includes(startId) && startId - (width * 2) === targetId) ||
+                    startId - width === targetId ||
+                    (startId - width - 1 === targetId && document.querySelectorAll(`[square-id="${startId - width - 1}"]`)[0]?.firstChild?.getAttribute("class") === "piece") ||
+                    (startId - width + 1 === targetId && document.querySelectorAll(`[square-id="${startId - width + 1}"]`)[0]?.firstChild?.getAttribute("class") === "piece")
+                ) {
+                    return true
+                }
+            } else {
+                const starterRow = [8,9,10,11,12,13,14,15];
+                if (
+                    (starterRow.includes(startId) && startId + (width * 2) === targetId) ||
+                    startId + width === targetId ||
+                    (startId + width - 1 === targetId && document.querySelectorAll(`[square-id="${startId + width - 1}"]`)[0]?.firstChild?.getAttribute("class") === "piece") ||
+                    (startId + width + 1 === targetId && document.querySelectorAll(`[square-id="${startId + width + 1}"]`)[0]?.firstChild?.getAttribute("class") === "piece")
+                ) {
+                    return true
+                }
+            }
+            break;
+        case piece.includes("knight") :
+            if (
+                startId + (width * 2) - 1 === targetId ||
+                startId + (width * 2) + 1 === targetId ||
+                startId + width - 2 === targetId ||
+                startId + width + 2 === targetId ||
+                startId - (width * 2) - 1 === targetId ||
+                startId - (width * 2) + 1 === targetId ||
+                startId - width - 2 === targetId ||
+                startId - width + 2 === targetId
+            ) {
+                return true
+            }
+            break;
+        case piece.includes("bishop") :
+            
+            return true
+        case piece.includes("rook") :
+            return true
+        case piece.includes("queen") :
+            return true
+        case piece.includes("king") :
+            return true
+    }
+
+    console.log("out of piece validation switch statement")
+    return false
+}
+
+function temporaryMessage(msg) {
+    infoDisplay.textContent = msg;
+    setTimeout(() => infoDisplay.textContent = "", 2000)
 }
 
 function dragStart(e) {
@@ -82,17 +161,47 @@ function dragOver(e) {
 
 function dragDrop(e) {
     e.stopPropagation();
-    const taken = e.target.classList.contains('piece');
 
-    // e.target.parentNode.append(draggedElement);
-    // e.target.remove();
+    const correctTurn = draggedElement.getAttribute('id').includes(playerTurn);
+    if (!correctTurn) {
+        temporaryMessage("not your turn buddy");
+        return
+    }
+    
+    const valid = checkIfValidMove(e.target);
+    if (!valid) {
+        //console.log("taken", taken, "opp", opponent, "takenBy", takenByOpponent, "v", valid);
+        temporaryMessage("invalid move");
+        return
+    }
+
+    const taken = e.target.parentNode.getAttribute("class")?.includes("piece");//?.contains('piece');
+    const opponent = playerTurn === "light" ? "dark" : "light";
+    const containsOpponent = e.target.parentNode.getAttribute("id").includes(opponent);
+    if (taken && !containsOpponent) {
+        temporaryMessage("invalid move");
+        return
+    }    
+    
+    // send move to server for validation here, if error return
+
+    if (taken && containsOpponent) {
+        const square = e.target.parentNode.parentNode;
+        e.target.parentNode.remove();
+        square.append(draggedElement);
+    } else {
+        e.target.append(draggedElement);
+    }
+    
     changePlayer();
 }
 
-// createBoard()
-createBoard("light");
 
-const allSquares = document.querySelectorAll("#gameboard .square");
+
+// start
+createBoard("dark");
+
+const allSquares = document.querySelectorAll(".square");
 
 allSquares.forEach( square => {
     square.addEventListener('dragstart', dragStart);
