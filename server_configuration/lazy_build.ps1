995267e9d4a4a1e999c275de1f0ebc1c4017199e
Michael Governanti <michaelgovernanti@gmail.com>
2025-02-22 06:53:43 -0500

    builds

--- server_configuration/lazy_build.ps1
+++ server_configuration/lazy_build.ps1
@@ -1,11 +1,22 @@
-# vars check
-$DropletIP = $env:BADCHESSDROPLETIP
-if ( -not $DropletIP || $DropletIP -eq "" ) {
-    Write-Output '$env:BADCHESSDROPLETIP environment variable missing
-    set with [Environment]::SetEnvironmentVariable("VariableName", "VariableValue", "User")'
+
+if ( (Read-Host "link up and build homie?[y/n]") -eq "y" ) {
+    # build new droplet if needed
+    pulumi refresh -C .\build\
+    pulumi up -C .\build
+
+    Write-Output "take that ip to cf homie"
     exit
 }
 
+# vars check
+$strArr = (pulumi stack output -C .\build | Select-String "dropletIP") -split "\s+"
+$DropletIP = $strArr[$strArr.Length - 1]
+if ( -not $DropletIP || $DropletIP -eq "" ) {
+    Write-Output "missing dropletIP output from pulumi"
+    exit
+}
+
+Write-Output "connecting to droplet ip $DropletIP"
 $BadChessPort = "8080"
 
 # build go binary for target system
@@ -18,14 +29,20 @@ $env:GOOS = $os; $env:GOARCH = $arch
 
 # template Caddyfile
 $CaddyTemplate = @"
-http://$DropletIP {
-        respond /metrics/* "Not Permitted" 403
-        reverse_proxy localhost:$BadChessPort
+{
+    email badchessmgmt@gmail.com
+}
+
+http://bad-chess.com {
+    respond /metrics/* "Not Permitted" 403
+    reverse_proxy localhost:$BadChessPort
 }
 "@
 
-$CaddyTemplate | Out-File -FilePath "./remote/Caddyfile" -Force
+#$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+#[System.IO.File]::WriteAllLines("./remote/Caddyfile", $CaddyTemplate, $Utf8NoBomEncoding)
+$CaddyTemplate | Out-File -Encoding utf8 -FilePath ".\remote\Caddyfile" -Force
 
 # copy binary & setup files to server
-scp -r ./remote ("root@{0}:~/" -f $DropletIP)
+scp -r .\remote ("root@{0}:~/" -f $DropletIP)
 ssh -t ("root@{0}" -f $DropletIP) "chmod +x ~/remote/server_setup.sh ~/remote/bad-chess-server && ~/remote/server_setup.sh"
--- server_configuration/remote/server_setup.sh
+++ server_configuration/remote/server_setup.sh
@@ -34,7 +34,7 @@ apt --yes install fail2ban
 
 # install & configure caddy
 # https://caddyserver.com/docs/install#debian-ubuntu-raspbian
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl libnss3-tools
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
