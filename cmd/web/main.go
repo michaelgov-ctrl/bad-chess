e1909605949455b6cdaa6e42cc652e2e1f88108f
Michael Governanti <michaelgovernanti@gmail.com>
2025-02-27 15:48:58 -0500

    o11y

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -19,7 +19,10 @@ import (
 
 type config struct {
 	port     int
+	lokiPort int
 	logLevel string
+	cert     string
+	key      string
 	cors     struct {
 		trustedOrigins []string
 	}
@@ -39,25 +42,23 @@ type application struct {
 func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 8080, "API server port")
+
+	flag.IntVar(&cfg.lokiPort, "loki-port", 0, "Port of local loki instace")
 	flag.StringVar(&cfg.logLevel, "log-level", "error", "Logging level (trace|debug|info|warning|error)")
+
+	flag.StringVar(&cfg.cert, "cert", "", "File containing cert for tls")
+	flag.StringVar(&cfg.key, "key", "", "File containing key for tls")
 
 	flag.Func("cors-trusted-origins", "Trusted CORS origins (space seperated)", func(val string) error {
 		cfg.cors.trustedOrigins = strings.Fields(val)
 		return nil
 	})
 
-	var cert, key string
-	flag.StringVar(&cert, "cert", "", "File containing cert for tls")
-	flag.StringVar(&key, "key", "", "File containing key for tls")
-
-	var lokiPort int
-	flag.IntVar(&lokiPort, "loki-port", 0, "Port of local loki instace")
-
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: logLevel(cfg.logLevel)}))
-	if lokiPort != 0 {
-		logger = slogloki.NewLokiLogger(fmt.Sprintf("http://localhost:%d/loki/api/v1/push", lokiPort), logLevel(cfg.logLevel))
+	if cfg.lokiPort != 0 {
+		logger = slogloki.NewLokiLogger(fmt.Sprintf("http://localhost:%d/loki/api/v1/push", cfg.lokiPort), logLevel(cfg.logLevel))
 	}
 
 	templateCache, err := newTemplateCache()
@@ -87,7 +88,7 @@ func main() {
 		metricsRegistry: registry,
 	}
 
-	if err := app.serve(cert, key); err != nil {
+	if err := app.serve(cfg.cert, cfg.key); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
